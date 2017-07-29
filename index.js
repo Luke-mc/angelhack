c0b2ab0a25caf5d4884a09a5e49c15e72f052e80
Luke-mc <luke_mcglynn@yahoo.com>
2017-07-29 11:59:50 -1000

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function initMap() {
+  var myLatLng = {lat: 21.385262, lng: -157.92475};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
+
+initMap();
